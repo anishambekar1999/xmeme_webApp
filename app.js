af18196ed1c42b90fe7b0a824cb670b7ac972e8b
Anish Ambekar <49511019+anishambekar1999@users.noreply.github.com>
2021-02-14 22:24:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -118,6 +118,6 @@ app.delete("/memes/:id",function(req,res){
     });
 });
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Server Started..!!");
 });
